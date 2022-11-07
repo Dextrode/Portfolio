e1f436319fab435a99847b411a0749ff1063c3d3
Gregory Osborne <gregoryosborne101@gmail.com>
2022-11-06 22:48:34 -0800

    Adjust htaccess and removing unneeded function

--- public/index.php
+++ public/index.php
@@ -7,17 +7,17 @@ require_once('../init.php');
 <html lang="en">
 <head>
     <!-- ** Head ** -->
-    <?php require(h('partials/global-partials/head.php')); ?>
+    <?php require('partials/global-partials/head.php'); ?>
 </head>
 
 <body class="bg-primary" data-aos-easing="ease" data-aos-duration="400" data-aos-delay="0">
     <header>
         <!-- ** Header ** -->
-        <?php require(h('partials/global-partials/header.php')); ?>
+        <?php require('partials/global-partials/header.php'); ?>
     </header>
     <main>
         <!-- ** Banner ** -->
-        <?php require(h('partials/home-partials/banner.php')); ?>
+        <?php require('partials/home-partials/banner.php'); ?>
 
 
         <!-- ** Artifact cards ** -->
@@ -26,24 +26,24 @@ require_once('../init.php');
             <div class="row g-3 d-flex justify-content-center">
                 <?php foreach($portfolio_artifacts as $portfolio_artifact): ?>
                     <!-- The file that the property values are looped through -->
-                    <?php require(h('partials/artifact-partials/cards/card1.php')); ?>
+                    <?php require('partials/artifact-partials/cards/card1.php'); ?>
                     <!-- Ends the loop -->
                 <?php endforeach; ?>
             </div>
         </div>
 
         <!-- ** Skills ** -->
-        <?php require(h('partials/home-partials/skills.php')); ?>
+        <?php require('partials/home-partials/skills.php'); ?>
 
         <!-- ** About ** -->
-        <?php require(h('partials/home-partials/about-section.php')); ?>
+        <?php require('partials/home-partials/about-section.php'); ?>
 
         <!-- ** Descriptors ** -->
-        <?php require(h('partials/home-partials/descriptors.php')); ?>
+        <?php require('partials/home-partials/descriptors.php'); ?>
     </main>
 
     <footer>
-        <?php require(h('partials/global-partials/footer.php')); ?>
+        <?php require('partials/global-partials/footer.php'); ?>
     </footer>
 </body>
 </html>
