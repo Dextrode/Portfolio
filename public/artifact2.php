74a09fbfd86f2508705a292f52d7dd8569fed7c0
Gregory Osborne <gregoryosborne101@gmail.com>
2022-11-06 18:29:43 -0800

    Switching to requires

--- public/artifact2.php
+++ public/artifact2.php
@@ -128,20 +128,7 @@ require_once('../init.php');
         </div>
     </main>
     <footer>
-        <div class="container my-4 p-4">
-            <div class="row pt-4">
-                <div class="col">
-                    <a href="https://www.facebook.com/gregory.osborne.98/" title="Link to Gregory Osborne Facebook Page" target="_blank"><img src="images/fb.svg" class="socials" alt="facebook logo icon" /></a>
-                    <a href="https://github.com/Dextrode" title="Link to Gregory Osborne Github Page" target="_blank"><img src="images/git.svg" class="socials" alt="Github  logo icon" /></a>
-                    <a href="https://www.linkedin.com/in/gregory-osborne-266b8b215/" title="Link to Gregory Osborne Linkedin Page" target="_blank"><img src="images/link.svg" class="socials" alt="Linkedin logo icon" /></a>
-                </div>
-            </div>
-            <div class="row pt-4">
-                <div class="col">
-                    <p>&copy; 2021 Gregory Osborne.</p>
-                </div>
-            </div>
-        </div>
+        <?php require(h('partials/global-partials/footer.php')); ?>
     </footer>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.min.js"></script>
     <script src="scripts/main.js"></script>
