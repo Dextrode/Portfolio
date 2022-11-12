9d425a928eb1cc67887b369eca567a2075fe5214
Gregory Osborne <gregoryosborne101@gmail.com>
2022-11-11 19:14:45 -0800

    Adjusting and removing unneededu files

--- public/partials/global-partials/head.php
+++ public/partials/global-partials/head.php
@@ -1,31 +1,23 @@
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!-- <title>Gregory Osborne | Front-end Developer in Vancouver</title>
-    <meta name="description" content="Gregory Osborne is a capable and decisive web developer based in Vancouver, BC" />
-    <meta name="keywords" content="Gregory Osborne, Web Developer, Front-End, Vancouver, BCIT, New Media, Portfolio" />
-    <meta name="author" content="Gregory Osborne" />
-    <meta property="og:title" content="Gregory Osborne | Front-end Developer in Vancouver" />
-    <meta property="og:url" content="https://gregoryosborne.ca" />
-    <meta property="og:description" content="Gregory Osborne is a capable and decisive web developer based in Vancouver, BC" /> -->
-    <!-- <link rel="shortcut icon" type="image/png" href="images/favicon.svg" /> -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600;700;800&family=Righteous&display=swap" rel="stylesheet" />
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<?php require('meta.php'); ?>
+<link rel="shortcut icon" type="image/png" href="images/favicon.svg" />
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600;700;800&family=Righteous&display=swap" rel="stylesheet" />
 
-    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
-    <link rel="stylesheet" href="styles/main.css"/>
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-82DC246HSR"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
+<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+<link rel="stylesheet" href="styles/main.css" />
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-82DC246HSR"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
 
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag("js", new Date());
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
 
-        gtag("config", "G-82DC246HSR");
-    </script>
-</head>
+    gtag("config", "G-82DC246HSR");
+</script>
