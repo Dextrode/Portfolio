94ca5b1e82934b2a8756731580e1ad2280420490
Gregory Osborne <gregoryosborne101@gmail.com>
2022-11-14 02:52:31 -0800

    Update contact.php

--- public/partials/home-partials/contact.php
+++ public/partials/home-partials/contact.php
@@ -3,9 +3,9 @@
         <div class="col-0 col-lg-1"></div>
         <div class="col-12 col-lg-10">
             <div class="d-flex justify-content-around">
-            <a href="mailto:gregoryosborne101@gmail.com" target="_blank">Email Me</a>
+                <a href="mailto:gregoryosborne101@gmail.com" class="btn btn-secondary w-25" target="_blank">Email Me</a>
 
-            <a href="tel:+3062030509">Phone Me</a>
+                <a href="tel:+3062030509" class="btn btn-secondary w-25" target="_blank">Phone Me</a>
             </div>
         </div>
         <div class="col-0 col-lg-1"></div>
